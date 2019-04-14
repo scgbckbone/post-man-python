eaa35c1b5ad11017560cf7e0edc65354921943f7
andrej <andrej.virgovic@pipelinersales.com>
2019-04-14 13:17:15 +0200

    Improved modularity of config.

--- config.py
+++ config.py
@@ -1,23 +1,6 @@
-# logging setup
-log_path = None
-logger_name = None
+from config_dev import *
 
-# smtp server
-smtp_host = None
-smtp_port = None
-
-addr = None
-pwd = None
-
-# mail receivers !!list!!
-mail_receiver = None
-if mail_receiver is None:
-    mail_receiver = [addr]
-
-# targets
-target_log_file = "/mnt/blockchain/debug.log"
-target_dir = "/tmp/post-man-python"
-
-# message details
-subject = "bitcoind alert notification"
-attached_target_name = "bitcoin_debug.log"
+try:
+    from config_prod import *
+except:
+    pass
